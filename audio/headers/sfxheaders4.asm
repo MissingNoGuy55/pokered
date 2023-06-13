ab16032e2431f136b162f9771953e4783ed00ec4
Missi <MissingNoGuy55@gmail.com>
2023-06-13 03:25:47 -0700

    Added duplicated files for "Audio Engine 4" and "Music 4" bank

---
+++ audio/headers/sfxheaders4.asm
@@ -0,0 +1,718 @@
+SFX_Headers_4::
+	db $ff, $ff, $ff ; padding
+
+SFX_Noise_Instrument01_4::
+	channel_count 1
+	channel 8, SFX_Noise_Instrument01_4_Ch8
+
+SFX_Noise_Instrument02_4::
+	channel_count 1
+	channel 8, SFX_Noise_Instrument02_4_Ch8
+
+SFX_Noise_Instrument03_4::
+	channel_count 1
+	channel 8, SFX_Noise_Instrument03_4_Ch8
+
+SFX_Noise_Instrument04_4::
+	channel_count 1
+	channel 8, SFX_Noise_Instrument04_4_Ch8
+
+SFX_Noise_Instrument05_4::
+	channel_count 1
+	channel 8, SFX_Noise_Instrument05_4_Ch8
+
+SFX_Noise_Instrument06_4::
+	channel_count 1
+	channel 8, SFX_Noise_Instrument06_4_Ch8
+
+SFX_Noise_Instrument07_4::
+	channel_count 1
+	channel 8, SFX_Noise_Instrument07_4_Ch8
+
+SFX_Noise_Instrument08_4::
+	channel_count 1
+	channel 8, SFX_Noise_Instrument08_4_Ch8
+
+SFX_Noise_Instrument09_4::
+	channel_count 1
+	channel 8, SFX_Noise_Instrument09_4_Ch8
+
+SFX_Noise_Instrument10_4::
+	channel_count 1
+	channel 8, SFX_Noise_Instrument10_4_Ch8
+
+SFX_Noise_Instrument11_4::
+	channel_count 1
+	channel 8, SFX_Noise_Instrument11_4_Ch8
+
+SFX_Noise_Instrument12_4::
+	channel_count 1
+	channel 8, SFX_Noise_Instrument12_4_Ch8
+
+SFX_Noise_Instrument13_4::
+	channel_count 1
+	channel 8, SFX_Noise_Instrument13_4_Ch8
+
+SFX_Noise_Instrument14_4::
+	channel_count 1
+	channel 8, SFX_Noise_Instrument14_4_Ch8
+
+SFX_Noise_Instrument15_4::
+	channel_count 1
+	channel 8, SFX_Noise_Instrument15_4_Ch8
+
+SFX_Noise_Instrument16_4::
+	channel_count 1
+	channel 8, SFX_Noise_Instrument16_4_Ch8
+
+SFX_Noise_Instrument17_4::
+	channel_count 1
+	channel 8, SFX_Noise_Instrument17_4_Ch8
+
+SFX_Noise_Instrument18_4::
+	channel_count 1
+	channel 8, SFX_Noise_Instrument18_4_Ch8
+
+SFX_Noise_Instrument19_4::
+	channel_count 1
+	channel 8, SFX_Noise_Instrument19_4_Ch8
+
+SFX_Cry00_4::
+	channel_count 3
+	channel 5, SFX_Cry00_4_Ch5
+	channel 6, SFX_Cry00_4_Ch6
+	channel 8, SFX_Cry00_4_Ch8
+
+SFX_Cry01_4::
+	channel_count 3
+	channel 5, SFX_Cry01_4_Ch5
+	channel 6, SFX_Cry01_4_Ch6
+	channel 8, SFX_Cry01_4_Ch8
+
+SFX_Cry02_4::
+	channel_count 3
+	channel 5, SFX_Cry02_4_Ch5
+	channel 6, SFX_Cry02_4_Ch6
+	channel 8, SFX_Cry02_4_Ch8
+
+SFX_Cry03_4::
+	channel_count 3
+	channel 5, SFX_Cry03_4_Ch5
+	channel 6, SFX_Cry03_4_Ch6
+	channel 8, SFX_Cry03_4_Ch8
+
+SFX_Cry04_4::
+	channel_count 3
+	channel 5, SFX_Cry04_4_Ch5
+	channel 6, SFX_Cry04_4_Ch6
+	channel 8, SFX_Cry04_4_Ch8
+
+SFX_Cry05_4::
+	channel_count 3
+	channel 5, SFX_Cry05_4_Ch5
+	channel 6, SFX_Cry05_4_Ch6
+	channel 8, SFX_Cry05_4_Ch8
+
+SFX_Cry06_4::
+	channel_count 3
+	channel 5, SFX_Cry06_4_Ch5
+	channel 6, SFX_Cry06_4_Ch6
+	channel 8, SFX_Cry06_4_Ch8
+
+SFX_Cry07_4::
+	channel_count 3
+	channel 5, SFX_Cry07_4_Ch5
+	channel 6, SFX_Cry07_4_Ch6
+	channel 8, SFX_Cry07_4_Ch8
+
+SFX_Cry08_4::
+	channel_count 3
+	channel 5, SFX_Cry08_4_Ch5
+	channel 6, SFX_Cry08_4_Ch6
+	channel 8, SFX_Cry08_4_Ch8
+
+SFX_Cry09_4::
+	channel_count 3
+	channel 5, SFX_Cry09_4_Ch5
+	channel 6, SFX_Cry09_4_Ch6
+	channel 8, SFX_Cry09_4_Ch8
+
+SFX_Cry0A_4::
+	channel_count 3
+	channel 5, SFX_Cry0A_4_Ch5
+	channel 6, SFX_Cry0A_4_Ch6
+	channel 8, SFX_Cry0A_4_Ch8
+
+SFX_Cry0B_4::
+	channel_count 3
+	channel 5, SFX_Cry0B_4_Ch5
+	channel 6, SFX_Cry0B_4_Ch6
+	channel 8, SFX_Cry0B_4_Ch8
+
+SFX_Cry0C_4::
+	channel_count 3
+	channel 5, SFX_Cry0C_4_Ch5
+	channel 6, SFX_Cry0C_4_Ch6
+	channel 8, SFX_Cry0C_4_Ch8
+
+SFX_Cry0D_4::
+	channel_count 3
+	channel 5, SFX_Cry0D_4_Ch5
+	channel 6, SFX_Cry0D_4_Ch6
+	channel 8, SFX_Cry0D_4_Ch8
+
+SFX_Cry0E_4::
+	channel_count 3
+	channel 5, SFX_Cry0E_4_Ch5
+	channel 6, SFX_Cry0E_4_Ch6
+	channel 8, SFX_Cry0E_4_Ch8
+
+SFX_Cry0F_4::
+	channel_count 3
+	channel 5, SFX_Cry0F_4_Ch5
+	channel 6, SFX_Cry0F_4_Ch6
+	channel 8, SFX_Cry0F_4_Ch8
+
+SFX_Cry10_4::
+	channel_count 3
+	channel 5, SFX_Cry10_4_Ch5
+	channel 6, SFX_Cry10_4_Ch6
+	channel 8, SFX_Cry10_4_Ch8
+
+SFX_Cry11_4::
+	channel_count 3
+	channel 5, SFX_Cry11_4_Ch5
+	channel 6, SFX_Cry11_4_Ch6
+	channel 8, SFX_Cry11_4_Ch8
+
+SFX_Cry12_4::
+	channel_count 3
+	channel 5, SFX_Cry12_4_Ch5
+	channel 6, SFX_Cry12_4_Ch6
+	channel 8, SFX_Cry12_4_Ch8
+
+SFX_Cry13_4::
+	channel_count 3
+	channel 5, SFX_Cry13_4_Ch5
+	channel 6, SFX_Cry13_4_Ch6
+	channel 8, SFX_Cry13_4_Ch8
+
+SFX_Cry14_4::
+	channel_count 3
+	channel 5, SFX_Cry14_4_Ch5
+	channel 6, SFX_Cry14_4_Ch6
+	channel 8, SFX_Cry14_4_Ch8
+
+SFX_Cry15_4::
+	channel_count 3
+	channel 5, SFX_Cry15_4_Ch5
+	channel 6, SFX_Cry15_4_Ch6
+	channel 8, SFX_Cry15_4_Ch8
+
+SFX_Cry16_4::
+	channel_count 3
+	channel 5, SFX_Cry16_4_Ch5
+	channel 6, SFX_Cry16_4_Ch6
+	channel 8, SFX_Cry16_4_Ch8
+
+SFX_Cry17_4::
+	channel_count 3
+	channel 5, SFX_Cry17_4_Ch5
+	channel 6, SFX_Cry17_4_Ch6
+	channel 8, SFX_Cry17_4_Ch8
+
+SFX_Cry18_4::
+	channel_count 3
+	channel 5, SFX_Cry18_4_Ch5
+	channel 6, SFX_Cry18_4_Ch6
+	channel 8, SFX_Cry18_4_Ch8
+
+SFX_Cry19_4::
+	channel_count 3
+	channel 5, SFX_Cry19_4_Ch5
+	channel 6, SFX_Cry19_4_Ch6
+	channel 8, SFX_Cry19_4_Ch8
+
+SFX_Cry1A_4::
+	channel_count 3
+	channel 5, SFX_Cry1A_4_Ch5
+	channel 6, SFX_Cry1A_4_Ch6
+	channel 8, SFX_Cry1A_4_Ch8
+
+SFX_Cry1B_4::
+	channel_count 3
+	channel 5, SFX_Cry1B_4_Ch5
+	channel 6, SFX_Cry1B_4_Ch6
+	channel 8, SFX_Cry1B_4_Ch8
+
+SFX_Cry1C_4::
+	channel_count 3
+	channel 5, SFX_Cry1C_4_Ch5
+	channel 6, SFX_Cry1C_4_Ch6
+	channel 8, SFX_Cry1C_4_Ch8
+
+SFX_Cry1D_4::
+	channel_count 3
+	channel 5, SFX_Cry1D_4_Ch5
+	channel 6, SFX_Cry1D_4_Ch6
+	channel 8, SFX_Cry1D_4_Ch8
+
+SFX_Cry1E_4::
+	channel_count 3
+	channel 5, SFX_Cry1E_4_Ch5
+	channel 6, SFX_Cry1E_4_Ch6
+	channel 8, SFX_Cry1E_4_Ch8
+
+SFX_Cry1F_4::
+	channel_count 3
+	channel 5, SFX_Cry1F_4_Ch5
+	channel 6, SFX_Cry1F_4_Ch6
+	channel 8, SFX_Cry1F_4_Ch8
+
+SFX_Cry20_4::
+	channel_count 3
+	channel 5, SFX_Cry20_4_Ch5
+	channel 6, SFX_Cry20_4_Ch6
+	channel 8, SFX_Cry20_4_Ch8
+
+SFX_Cry21_4::
+	channel_count 3
+	channel 5, SFX_Cry21_4_Ch5
+	channel 6, SFX_Cry21_4_Ch6
+	channel 8, SFX_Cry21_4_Ch8
+
+SFX_Cry22_4::
+	channel_count 3
+	channel 5, SFX_Cry22_4_Ch5
+	channel 6, SFX_Cry22_4_Ch6
+	channel 8, SFX_Cry22_4_Ch8
+
+SFX_Cry23_4::
+	channel_count 3
+	channel 5, SFX_Cry23_4_Ch5
+	channel 6, SFX_Cry23_4_Ch6
+	channel 8, SFX_Cry23_4_Ch8
+
+SFX_Cry24_4::
+	channel_count 3
+	channel 5, SFX_Cry24_4_Ch5
+	channel 6, SFX_Cry24_4_Ch6
+	channel 8, SFX_Cry24_4_Ch8
+
+SFX_Cry25_4::
+	channel_count 3
+	channel 5, SFX_Cry25_4_Ch5
+	channel 6, SFX_Cry25_4_Ch6
+	channel 8, SFX_Cry25_4_Ch8
+
+SFX_Get_Item1_4::
+	channel_count 3
+	channel 5, SFX_Get_Item1_4_Ch5
+	channel 6, SFX_Get_Item1_4_Ch6
+	channel 7, SFX_Get_Item1_4_Ch7
+
+SFX_Get_Item2_4::
+	channel_count 3
+	channel 5, SFX_Get_Item2_4_Ch5
+	channel 6, SFX_Get_Item2_4_Ch6
+	channel 7, SFX_Get_Item2_4_Ch7
+
+SFX_Tink_4::
+	channel_count 1
+	channel 5, SFX_Tink_4_Ch5
+
+SFX_Heal_HP_4::
+	channel_count 1
+	channel 5, SFX_Heal_HP_4_Ch5
+
+SFX_Heal_Ailment_4::
+	channel_count 1
+	channel 5, SFX_Heal_Ailment_4_Ch5
+
+SFX_Start_Menu_4::
+	channel_count 1
+	channel 8, SFX_Start_Menu_4_Ch8
+
+SFX_Press_AB_4::
+	channel_count 1
+	channel 5, SFX_Press_AB_4_Ch5
+
+SFX_Pokedex_Rating_4::
+	channel_count 3
+	channel 5, SFX_Pokedex_Rating_4_Ch5
+	channel 6, SFX_Pokedex_Rating_4_Ch6
+	channel 7, SFX_Pokedex_Rating_4_Ch7
+
+SFX_Get_Key_Item_4::
+	channel_count 3
+	channel 5, SFX_Get_Key_Item_4_Ch5
+	channel 6, SFX_Get_Key_Item_4_Ch6
+	channel 7, SFX_Get_Key_Item_4_Ch7
+
+SFX_Poisoned_4::
+	channel_count 1
+	channel 5, SFX_Poisoned_4_Ch5
+
+SFX_Trade_Machine_4::
+	channel_count 1
+	channel 5, SFX_Trade_Machine_4_Ch5
+
+SFX_Turn_On_PC_4::
+	channel_count 1
+	channel 5, SFX_Turn_On_PC_4_Ch5
+
+SFX_Turn_Off_PC_4::
+	channel_count 1
+	channel 5, SFX_Turn_Off_PC_4_Ch5
+
+SFX_Enter_PC_4::
+	channel_count 1
+	channel 5, SFX_Enter_PC_4_Ch5
+
+SFX_Shrink_4::
+	channel_count 1
+	channel 5, SFX_Shrink_4_Ch5
+
+SFX_Switch_4::
+	channel_count 1
+	channel 5, SFX_Switch_4_Ch5
+
+SFX_Healing_Machine_4::
+	channel_count 1
+	channel 5, SFX_Healing_Machine_4_Ch5
+
+SFX_Teleport_Exit1_4::
+	channel_count 1
+	channel 5, SFX_Teleport_Exit1_4_Ch5
+
+SFX_Teleport_Enter1_4::
+	channel_count 1
+	channel 5, SFX_Teleport_Enter1_4_Ch5
+
+SFX_Teleport_Exit2_4::
+	channel_count 1
+	channel 5, SFX_Teleport_Exit2_4_Ch5
+
+SFX_Ledge_4::
+	channel_count 1
+	channel 5, SFX_Ledge_4_Ch5
+
+SFX_Teleport_Enter2_4::
+	channel_count 1
+	channel 8, SFX_Teleport_Enter2_4_Ch8
+
+SFX_Fly_4::
+	channel_count 1
+	channel 8, SFX_Fly_4_Ch8
+
+SFX_Denied_4::
+	channel_count 2
+	channel 5, SFX_Denied_4_Ch5
+	channel 6, SFX_Denied_4_Ch6
+
+SFX_Arrow_Tiles_4::
+	channel_count 1
+	channel 5, SFX_Arrow_Tiles_4_Ch5
+
+SFX_Push_Boulder_4::
+	channel_count 1
+	channel 8, SFX_Push_Boulder_4_Ch8
+
+SFX_SS_Anne_Horn_4::
+	channel_count 2
+	channel 5, SFX_SS_Anne_Horn_4_Ch5
+	channel 6, SFX_SS_Anne_Horn_4_Ch6
+
+SFX_Withdraw_Deposit_4::
+	channel_count 1
+	channel 5, SFX_Withdraw_Deposit_4_Ch5
+
+SFX_Cut_4::
+	channel_count 1
+	channel 8, SFX_Cut_4_Ch8
+
+SFX_Go_Inside_4::
+	channel_count 1
+	channel 8, SFX_Go_Inside_4_Ch8
+
+SFX_Swap_4::
+	channel_count 2
+	channel 5, SFX_Swap_4_Ch5
+	channel 6, SFX_Swap_4_Ch6
+
+SFX_59_4::
+	channel_count 2
+	channel 5, SFX_59_4_Ch5
+	channel 6, SFX_59_4_Ch6
+
+SFX_Purchase_4::
+	channel_count 2
+	channel 5, SFX_Purchase_4_Ch5
+	channel 6, SFX_Purchase_4_Ch6
+
+SFX_Collision_4::
+	channel_count 1
+	channel 5, SFX_Collision_4_Ch5
+
+SFX_Go_Outside_4::
+	channel_count 1
+	channel 8, SFX_Go_Outside_4_Ch8
+	
+SFX_Save_4::
+	channel_count 2
+	channel 5, SFX_Save_4_Ch5
+	channel 6, SFX_Save_4_Ch6
+	
+SFX_Ball_Toss_4::
+	channel_count 2
+	channel 5, SFX_Ball_Toss_4_Ch5
+	channel 6, SFX_Ball_Toss_4_Ch6
+
+SFX_Ball_Poof_4::
+	channel_count 2
+	channel 5, SFX_Ball_Poof_4_Ch5
+	channel 8, SFX_Ball_Poof_4_Ch8
+
+SFX_Faint_Thud_4::
+	channel_count 2
+	channel 5, SFX_Faint_Thud_4_Ch5
+	channel 8, SFX_Faint_Thud_4_Ch8
+
+SFX_Run_4::
+	channel_count 1
+	channel 8, SFX_Run_4_Ch8
+
+SFX_Dex_Page_Added_4::
+	channel_count 2
+	channel 5, SFX_Dex_Page_Added_4_Ch5
+	channel 6, SFX_Dex_Page_Added_4_Ch6
+
+SFX_Caught_Mon_4::
+	channel_count 3
+	channel 5, SFX_Caught_Mon_4_Ch5
+	channel 6, SFX_Caught_Mon_4_Ch6
+	channel 7, SFX_Caught_Mon_4_Ch7
+
+; SFX_Peck_4::
+	; channel_count 1
+	; channel 8, SFX_Peck_4_Ch8
+
+; SFX_Faint_Fall_4::
+	; channel_count 1
+	; channel 5, SFX_Faint_Fall_4_Ch5
+
+; SFX_Battle_09_4::
+	; channel_count 1
+	; channel 5, SFX_Battle_09_4_Ch5
+
+; SFX_Pound_4::
+	; channel_count 1
+	; channel 8, SFX_Pound_4_Ch8
+
+; SFX_Battle_0B_4::
+	; channel_count 1
+	; channel 8, SFX_Battle_0B_4_Ch8
+
+; SFX_Battle_0C_4::
+	; channel_count 1
+	; channel 8, SFX_Battle_0C_4_Ch8
+
+; SFX_Battle_0D_4::
+	; channel_count 1
+	; channel 8, SFX_Battle_0D_4_Ch8
+
+; SFX_Battle_0E_4::
+	; channel_count 1
+	; channel 8, SFX_Battle_0E_4_Ch8
+
+; SFX_Battle_0F_4::
+	; channel_count 1
+	; channel 8, SFX_Battle_0F_4_Ch8
+
+; SFX_Damage_4::
+	; channel_count 1
+	; channel 8, SFX_Damage_4_Ch8
+
+; SFX_Not_Very_Effective_4::
+	; channel_count 1
+	; channel 8, SFX_Not_Very_Effective_4_Ch8
+
+; SFX_Battle_12_4::
+	; channel_count 1
+	; channel 8, SFX_Battle_12_4_Ch8
+
+; SFX_Battle_13_4::
+	; channel_count 1
+	; channel 8, SFX_Battle_13_4_Ch8
+
+; SFX_Battle_14_4::
+	; channel_count 1
+	; channel 8, SFX_Battle_14_4_Ch8
+
+; SFX_Vine_Whip_4::
+	; channel_count 1
+	; channel 8, SFX_Vine_Whip_4_Ch8
+
+; SFX_Battle_16_4::
+	; channel_count 1
+	; channel 8, SFX_Battle_16_4_Ch8
+
+; SFX_Battle_17_4::
+	; channel_count 1
+	; channel 8, SFX_Battle_17_4_Ch8
+
+; SFX_Battle_18_4::
+	; channel_count 1
+	; channel 8, SFX_Battle_18_4_Ch8
+
+; SFX_Battle_19_4::
+	; channel_count 1
+	; channel 8, SFX_Battle_19_4_Ch8
+
+; SFX_Super_Effective_4::
+	; channel_count 1
+	; channel 8, SFX_Super_Effective_4_Ch8
+
+; SFX_Battle_1B_4::
+	; channel_count 1
+	; channel 8, SFX_Battle_1B_4_Ch8
+
+; SFX_Battle_1C_4::
+	; channel_count 1
+	; channel 8, SFX_Battle_1C_4_Ch8
+
+; SFX_Doubleslap_4::
+	; channel_count 1
+	; channel 8, SFX_Doubleslap_4_Ch8
+
+; SFX_Battle_1E_4::
+	; channel_count 2
+	; channel 5, SFX_Battle_1E_4_Ch5
+	; channel 8, SFX_Battle_1E_4_Ch8
+
+; SFX_Horn_Drill_4::
+	; channel_count 1
+	; channel 8, SFX_Horn_Drill_4_Ch8
+
+; SFX_Battle_20_4::
+	; channel_count 1
+	; channel 8, SFX_Battle_20_4_Ch8
+
+; SFX_Battle_21_4::
+	; channel_count 1
+	; channel 8, SFX_Battle_21_4_Ch8
+
+; SFX_Battle_22_4::
+	; channel_count 1
+	; channel 8, SFX_Battle_22_4_Ch8
+
+; SFX_Battle_23_4::
+	; channel_count 1
+	; channel 8, SFX_Battle_23_4_Ch8
+
+; SFX_Battle_24_4::
+	; channel_count 2
+	; channel 5, SFX_Battle_24_4_Ch5
+	; channel 8, SFX_Battle_24_4_Ch8
+
+; SFX_Battle_25_4::
+	; channel_count 1
+	; channel 8, SFX_Battle_25_4_Ch8
+
+; SFX_Battle_26_4::
+	; channel_count 1
+	; channel 8, SFX_Battle_26_4_Ch8
+
+; SFX_Battle_27_4::
+	; channel_count 3
+	; channel 5, SFX_Battle_27_4_Ch5
+	; channel 6, SFX_Battle_27_4_Ch6
+	; channel 8, SFX_Battle_27_4_Ch8
+
+; SFX_Battle_28_4::
+	; channel_count 3
+	; channel 5, SFX_Battle_28_4_Ch5
+	; channel 6, SFX_Battle_28_4_Ch6
+	; channel 8, SFX_Battle_28_4_Ch8
+
+; SFX_Battle_29_4::
+	; channel_count 2
+	; channel 5, SFX_Battle_29_4_Ch5
+	; channel 8, SFX_Battle_29_4_Ch8
+
+; SFX_Battle_2A_4::
+	; channel_count 3
+	; channel 5, SFX_Battle_2A_4_Ch5
+	; channel 6, SFX_Battle_2A_4_Ch6
+	; channel 8, SFX_Battle_2A_4_Ch8
+
+; SFX_Battle_2B_4::
+	; channel_count 2
+	; channel 5, SFX_Battle_2B_4_Ch5
+	; channel 8, SFX_Battle_2B_4_Ch8
+
+; SFX_Battle_2C_4::
+	; channel_count 3
+	; channel 5, SFX_Battle_2C_4_Ch5
+	; channel 6, SFX_Battle_2C_4_Ch6
+	; channel 8, SFX_Battle_2C_4_Ch8
+
+; SFX_Psybeam_4::
+	; channel_count 3
+	; channel 5, SFX_Psybeam_4_Ch5
+	; channel 6, SFX_Psybeam_4_Ch6
+	; channel 8, SFX_Psybeam_4_Ch8
+
+; SFX_Battle_2E_4::
+	; channel_count 3
+	; channel 5, SFX_Battle_2E_4_Ch5
+	; channel 6, SFX_Battle_2E_4_Ch6
+	; channel 8, SFX_Battle_2E_4_Ch8
+
+; SFX_Battle_2F_4::
+	; channel_count 3
+	; channel 5, SFX_Battle_2F_4_Ch5
+	; channel 6, SFX_Battle_2F_4_Ch6
+	; channel 8, SFX_Battle_2F_4_Ch8
+
+; SFX_Psychic_M_4::
+	; channel_count 3
+	; channel 5, SFX_Psychic_M_4_Ch5
+	; channel 6, SFX_Psychic_M_4_Ch6
+	; channel 8, SFX_Psychic_M_4_Ch8
+
+; SFX_Battle_31_4::
+	; channel_count 2
+	; channel 5, SFX_Battle_31_4_Ch5
+	; channel 6, SFX_Battle_31_4_Ch6
+
+; SFX_Battle_32_4::
+	; channel_count 2
+	; channel 5, SFX_Battle_32_4_Ch5
+	; channel 6, SFX_Battle_32_4_Ch6
+
+; SFX_Battle_33_4::
+	; channel_count 2
+	; channel 5, SFX_Battle_33_4_Ch5
+	; channel 6, SFX_Battle_33_4_Ch6
+
+; SFX_Battle_34_4::
+	; channel_count 3
+	; channel 5, SFX_Battle_34_4_Ch5
+	; channel 6, SFX_Battle_34_4_Ch6
+	; channel 8, SFX_Battle_34_4_Ch8
+
+; SFX_Battle_35_4::
+	; channel_count 2
+	; channel 5, SFX_Battle_35_4_Ch5
+	; channel 6, SFX_Battle_35_4_Ch6
+
+; SFX_Battle_36_4::
+	; channel_count 3
+	; channel 5, SFX_Battle_36_4_Ch5
+	; channel 6, SFX_Battle_36_4_Ch6
+	; channel 8, SFX_Battle_36_4_Ch8
+
+SFX_Silph_Scope_4::
+	channel_count 1
+	channel 5, SFX_Silph_Scope_4_Ch5
